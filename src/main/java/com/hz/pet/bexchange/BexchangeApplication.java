0d8e5c186be820248356693650c14956a41b2049
Herman Zamula <herman.zamula@gmail.com>
2016-06-09 14:36:40 +0300

    Add the project framework

---
+++ src/main/java/com/hz/pet/bexchange/BexchangeApplication.java
@@ -0,0 +1,12 @@
+package com.hz.pet.bexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BexchangeApplication.class, args);
+	}
+}
