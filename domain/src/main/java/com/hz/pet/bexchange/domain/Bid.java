a904a830797c76c7e80cb4bbc8cb2b5f9b688963
Herman Zamula <herman.zamula@gmail.com>
2016-06-13 14:22:22 +0300

    Add basic domain

---
+++ domain/src/main/java/com/hz/pet/bexchange/domain/Bid.java
@@ -0,0 +1,38 @@
+package com.hz.pet.bexchange.domain;
+
+import java.util.Date;
+
+/**
+ * @author Herman Zamula
+ */
+public interface Bid {
+
+    User getCreator();
+
+    Date getCreationTime();
+
+    String getCity();
+
+    String getCityRegion();
+
+    String getComment();
+
+    String getCurrency();
+
+    float getExchangeRate();
+
+    long getAmount();
+
+    BidType getBidType();
+
+    BidStatus getBidStatus();
+
+    enum BidType {
+        SELL, BUY
+    }
+
+    enum BidStatus {
+        ACTIVE, DONE
+    }
+
+}
---
+++ domain/src/main/java/com/hz/pet/bexchange/domain/User.java
@@ -0,0 +1,10 @@
+package com.hz.pet.bexchange.domain;
+
+/**
+ * @author Herman Zamula
+ */
+public interface User {
+
+    String getPhoneNumber();
+
+}
